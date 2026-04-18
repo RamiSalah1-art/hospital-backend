68ae69772a79329ff3bb139e84509a7f5a0dbe5d
RamiSalah1-art <ramiseryani1995@gmail.com>
2026-04-18 09:10:59 +0200

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { login, getProfile, register } from '../controllers/auth.controller';
+import { login, getProfile, register, deleteUser } from '../controllers/auth.controller';
 import { authenticate } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.delete('/users/:username', deleteUser);
 router.get('/profile', authenticate, getProfile);
 
 export default router;
